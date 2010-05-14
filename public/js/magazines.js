a9dcad5686364e7fed4222c8782a28edb63b2892
Randy Schmidt <randy@forge38.com>
2010-05-14 16:39:11 -0400

    Fixed indentation

--- public/js/magazines.js
+++ public/js/magazines.js
@@ -7,29 +7,29 @@ Hummingbird.getMagazines = function() {
 									 {sld: "twistmagazine", name: "Twist Magazine", key: 'twist'},
 									 {sld: "mmm-mag", name: "MMM Magazine", key: 'mmm'},
 									 {sld: "quizfest", name: "Quizfest", key: 'qf'}];
-   $.each(magazines, function() {
+	$.each(magazines, function() {
 		var editorialImage = "";
-     var name = this.name;
+		var name = this.name;
 
-     var magDiv = $("<div id='mag_" + this.key + "' data-id='" + this.key + "' class='sale'></div>");
-     magDiv.append("<h2>" + name + "</h2>");
+		var magDiv = $("<div id='mag_" + this.key + "' data-id='" + this.key + "' class='sale'></div>");
+		magDiv.append("<h2>" + name + "</h2>");
 
-     var graph = $("<div class='hummingbird_graph'></div>");
-     graph.append("<div class='req_s'><span class='value'>0</span> pages/sec</div>");
+		var graph = $("<div class='hummingbird_graph'></div>");
+		graph.append("<div class='req_s'><span class='value'>0</span> pages/sec</div>");
 
-     var canvas = $("<canvas width='185' height='70'></canvas>");
-     graph.append(canvas);
-     magDiv.append(graph);
-     $("#sales").append(magDiv);
-     var magGraph = new Hummingbird.Graph(graph, { ratePerSecond: 2, initialScope: 200, backgroundImage: editorialImage, showBackgroundBars: false });
-     Hummingbird.magGraphs[this.key] = magGraph;
-   });
+		var canvas = $("<canvas width='185' height='70'></canvas>");
+		graph.append(canvas);
+		magDiv.append(graph);
+		$("#sales").append(magDiv);
+		var magGraph = new Hummingbird.Graph(graph, { ratePerSecond: 2, initialScope: 200, backgroundImage: editorialImage, showBackgroundBars: false });
+		Hummingbird.magGraphs[this.key] = magGraph;
+	});
 
-   var canvasWidth = $("#log canvas").width() - 8;
-   var magsPerRow = Math.floor(canvasWidth / 190);
-   var extraSpace = canvasWidth % 190;
-   var extraSpacePerSale = (extraSpace + 10) / (magsPerRow - 1);
+	var canvasWidth = $("#log canvas").width() - 8;
+	var magsPerRow = Math.floor(canvasWidth / 190);
+	var extraSpace = canvasWidth % 190;
+	var extraSpacePerSale = (extraSpace + 10) / (magsPerRow - 1);
 
-   $("#sales div.sale").css({marginRight: extraSpacePerSale + 10});
-   $("body").css({minWidth: $("#log").width()});
+	$("#sales div.sale").css({marginRight: extraSpacePerSale + 10});
+	$("body").css({minWidth: $("#log").width()});
 };
